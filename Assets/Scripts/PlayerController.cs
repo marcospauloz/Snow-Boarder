6b35419017040ff0a0b7d01cee50b7b54c40c3b9
Marcos Zanelatto <marcospaulozanelatto@live.com>
2022-08-02 07:44:58 -0300

    Create project

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        var rb2d = GetComponent<Rigidbody2D>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Resources.md
@@ -0,0 +1,6 @@
+Sprite Shape
+Cinemachine
+    Manage multiple cameras in our scene
+    Easily create rules for our cameras
+Surface effector 2d
+    Add a conveyor to push things. Added in the level sprite shape. Must set the Edge collider 2d to be used by effector
